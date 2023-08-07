181cff1bb6a48873cc4a47c5ac68a95ce8132043
bhaveshgadag <bhaveshgadag46@gmail.com>
2023-08-07 20:51:48 +0530

    send detected object via email

--- send_email.py
+++ send_email.py
@@ -1,3 +1,30 @@
+import smtplib
+import imghdr
+from email.message import EmailMessage
+import os
+
+PASSWORD = os.getenv("GMAIL_APP_PASSWORD")
+SENDER = os.getenv("EMAIL")
+RECEIVER = os.getenv("EMAIL")
+
+
 def send_email(image):
-    print("Email was sent.")
-    print(image)
+    email_content = EmailMessage()
+    email_content["Subject"] = "Object detected"
+    email_content.set_content("A moving object was detected")
+
+    with open(image, "rb") as file:
+        content = file.read()
+
+    email_content.add_attachment(content, maintype="image", subtype=imghdr.what(None, content))
+
+    gmail = smtplib.SMTP("smtp.gmail.com")
+    gmail.ehlo()
+    gmail.starttls()
+    gmail.login(SENDER, PASSWORD)
+    gmail.sendmail(SENDER, RECEIVER, email_content.as_string())
+    gmail.quit()
+
+
+if __name__ == '__main__':
+    send_email("images/image_190.png")
