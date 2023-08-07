868b407f50bf1df5b66b799c671679b941eb1865
bhaveshgadag <bhaveshgadag46@gmail.com>
2023-08-07 12:08:01 +0530

    get camera feed

---
+++ main.py
@@ -0,0 +1,14 @@
+import cv2
+
+camera = cv2.VideoCapture(0)
+
+while True:
+    check, frame = camera.read()
+
+    cv2.imshow("Video", frame)
+
+    key = cv2.waitKey(1)
+    if key == ord('q'):
+        break
+
+camera.release()
